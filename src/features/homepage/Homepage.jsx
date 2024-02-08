c6073a5b6b2c06810062bd5e8ecc02033b73d849
chronus091 <gautambusiness777@gmail.com>
2024-02-08 10:26:24 +0530

    simply

--- src/features/homepage/Homepage.jsx
+++ src/features/homepage/Homepage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Featured from '../../components/featured/Featured'
 
+
 const Homepage = () => {
   return (
     <div className='homepage'>
       <Featured/>
+      
     </div>
   )
 }
