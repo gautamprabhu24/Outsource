337e3de765ed6b94d1610de687cd43f21616b791
chronus091 <gautambusiness777@gmail.com>
2024-02-09 12:13:38 +0530

    fifth push

--- src/components/slider/Slide.jsx
+++ src/components/slider/Slide.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { FaCaretLeft, FaCaretRight } from "react-icons/fa6";
 
 const Slide = ({children}) => {
   const SampleNextArrow = (props) => {
@@ -11,7 +12,7 @@ const Slide = ({children}) => {
         className={className} 
         style={{
           ...style,
-          display: "block",background: "grey",borderRadius: "50%",cursor: "pointer",opacity: "0.3",width:"19px",height:"20px"
+          display: "block",background: "grey",borderRadius: "50%",cursor: "pointer",width:"19px",height:"20px"
         }}
         onClick={onClick}
       />
@@ -23,7 +24,7 @@ const Slide = ({children}) => {
     return (
       <div
         className={className}
-        style={{ ...style, display: "block", background: "grey", borderRadius:"20px",cursor:"pointer",opacity: "0.3",width:"16px",height:"17px" }}
+        style={{ ...style, display: "block", background: "grey", borderRadius:"20px",cursor:"pointer", width:"19px",height:"20px" }}
         onClick={onClick}
       />
     );
@@ -37,7 +38,7 @@ const Slide = ({children}) => {
     initialSlide: 0,
     nextArrow: <SampleNextArrow />,
     prevArrow: <SamplePrevArrow />,
-    centerMode: true,
+    centerMode: false,
     centerPadding: '0',
     responsive: [
       {
